4ce21633891d70c09430cf5f2659e305dba3291a
Hans Foster <fosterhans@gmail.com>
2019-10-27 22:18:10 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -8,4 +8,37 @@ end
 
 def sort_array_char_count(array)
   array.sort()
+end
 
+def swap_elements(array)
+  second_element = array[1]
+  array[1] = array[2]
+  array[2] = second_element
+  array
+end
+
+def reverse_array(array)
+  array.sort
+  array.reverse
+end
+
+def kesha_maker(array)
+  new_array = []
+  array.each do |string|
+    new_array << string
+  end
+end
+
+def find_a(array)
+  array.select do |string|
+    string.start_with?("a")
+  end
+end
+
+def add_s(array)
+  array.each_with_index.collect{|element, index|
+    element = element + s if index != 2
+  end
+end
+
+    
